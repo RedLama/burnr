2ad42fe51dedd8a583b3cc785e8d8ffb4a88c230
Damien <damien@redlama.fr>
2015-09-18 13:34:52 +0200

    add 10 principles

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   resources :newsletters, only: :create
 
+  get "10-principles", to: "pages#show", defaults: { id: "10-principles"}
+
   namespace :admin do
     resources :newsletters, only: [:index, :destroy]
     resources :users, only: [:index, :destroy]
