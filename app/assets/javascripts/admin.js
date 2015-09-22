9443aee9d818f1b94964112d5a0b578808edc9ca
Damien <damien@redlama.fr>
2015-09-22 16:54:19 +0200

    add localized date picker for publish date

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,4 +1,17 @@
 //= require jquery
 //= require jquery_ujs
+//= require jquery-ui/datepicker
+//= require jquery-ui/datepicker-fr
+//= require jquery-ui/datepicker-es
+//= require jquery-ui/
 //= require twitter/bootstrap
 //= require bootstrap
+//= require turbolinks
+
+$(document).on('page:change', function(event) {
+  if ($('.datepicker').length >= 0) {
+    locale = $('.datepicker').data('locale');
+    $(".datepicker").datepicker( $.datepicker.regional[ locale == "en" ? "" : locale ] );
+  }
+
+});
